548cafc9060506fea84b07f552401f8c8d14a62e
Rohit Ghate <99409706+rohitghate29@users.noreply.github.com>
2024-07-19 13:42:19 +0530

    Update Home.jsx

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -209,9 +209,11 @@ function Home() {
 
           <div className="mt-12 text-center">
             <Link
-              to = { user.isSignedIn
-                  ? "https://ai-resume-builder-mu.vercel.app/dashboard" 
-                  : "https://ai-resume-builder-mu.vercel.app/auth/sign-in"}>
+              to={
+                user.isSignedIn
+                  ? `http://localhost:5173/dashboard`
+                  : `http://localhost:5173/auth/sign-in`
+              }
               className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
             >
               Get Started Today
